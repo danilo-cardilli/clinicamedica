89326f078610f8400d9acf22e23a271cdec0728d
danilo-cardilli <danilocardilli@yahoo.com.br>
2024-10-08 17:02:50 -0300

    Atualização da a Ordenacao

--- src/main/java/com/clinica/clinicamedica/controller/MedicoController.java
+++ src/main/java/com/clinica/clinicamedica/controller/MedicoController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,11 +23,10 @@ public class MedicoController {
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-
         repository.save(new Medico(dados));
     }
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
